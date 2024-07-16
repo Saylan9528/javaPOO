b21a8c1184bc1058868ef11aa12168e91e657e7c
PHMM <paulo.henrique9528@gmail.com>
2024-07-16 15:59:04 -0300

    trabalhando com heranca e Polimorfismo

---
+++ ZOO/src/heranca/com/java/Animal.java
@@ -0,0 +1,29 @@
+
+package heranca.com.java;
+
+/**
+ *
+ * @author phmm
+ */
+public class Animal {
+    
+    private String alimenta;
+    private String anda;
+    
+    
+    public String getAnda(){
+        return anda;
+    }
+    
+    public void setAnda(String anda){
+        this.anda = anda;
+    }
+    
+    public String getAlimenta(){
+        return alimenta;
+    }
+    
+    public void setAlimenta(String alimenta){
+        this.alimenta = alimenta;
+    }
+}
